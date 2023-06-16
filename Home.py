cc83611d5e68f961fcc5d0206148e201d8c6b637
Sunchonc <sunchoco.73@gmail.com>
2023-06-16 14:58:12 +0700

    .commit

--- Home.py
+++ Home.py
@@ -2,3 +2,16 @@ import streamlit as st
 
 st.header('natthasut')
 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Versicolor")
+   st.image("./pic/Iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./pic/Iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./pic/Iris3.jpg")
